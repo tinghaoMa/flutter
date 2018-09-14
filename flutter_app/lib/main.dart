000ef448d3e3a032621499b092c0202a16dcc0cb
tinghaoma <tinghaoma@sohu-inc.com>
2018-09-14 13:56:44 +0800

    第一个RandomWords Stateful widget

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -6,7 +6,7 @@ void main() => runApp(new MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final wordPair=new WordPair.random();
+//    final wordPair = new WordPair.random();
     return new MaterialApp(
       title: 'Welcome to Flutter',
       home: new Scaffold(
@@ -14,9 +14,23 @@ class MyApp extends StatelessWidget {
           title: new Text('Welcome to Flutter'),
         ),
         body: new Center(
-          child: new Text(wordPair.asPascalCase),
+          child: new RandomWord(),
         ),
       ),
     );
+  }
+}
+
+class RandomWord extends StatefulWidget {
+  @override
+  createState() =>new RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWord> {
+
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = new WordPair.random();
+    return  new Text(wordPair.asPascalCase);
   }
 }
